f87efc80e801fff4c73b1b6ca98fbd725253a78c
Lee Jae-Seong <74041698+dlwo9503@users.noreply.github.com>
2021-04-02 02:07:14 +0900

    Material UI 적용 및 withStyles 사용

--- src/App.js
+++ src/App.js
@@ -1,55 +1,78 @@
+// https://material-ui.com/components/tables/#table // 테이블 디자인 소스 참고
+
 import logo from './logo.svg';
 import './App.css';
 import Customer from './components/Customer'; // export 한 Customer 불러오기
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper'; // 컴포넌트의 외부를 감싸기 위함
+import { Component } from 'react';
+
+const styles = theme => ({ // 머테리얼 유아이에다 css 같이 사용
+  root: { // root class css
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto" // x축으로 오버플로우가 발생할 수 있도록 처리하는거임
+  },
+  table: { // table class css
+    minWidth: 1080 // 테이블은 적어도 1080px만큼 자리를 차지하도록 함
+  }
+})
 
 const customers = [
-{
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name': '이재성',
-  'birthday': '950328',
-  'gender': '남자',
-  'job': '개발자'
-},
-{
-  'id': 2,
-  'image': 'https://placeimg.com/64/64/2',
-  'name': '김지영',
-  'birthday': '920825',
-  'gender': '여자',
-  'job': '공무원'
-},
-{
-  'id': 3,
-  'image': 'https://placeimg.com/64/64/3',
-  'name': '이재영',
-  'birthday': '920722',
-  'gender': '남자',
-  'job': '자영업자'
-}
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '이재성',
+    'birthday': '950328',
+    'gender': '남자',
+    'job': '개발자'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '김지영',
+    'birthday': '920825',
+    'gender': '여자',
+    'job': '공무원'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '이재영',
+    'birthday': '920722',
+    'gender': '남자',
+    'job': '자영업자'
+  }
 ]
 
-function App() {
-  return (
-    <div>
-      {
-        customers.map(c => {
-          return (
-            <Customer
-            key={c.id}
-            id={c.id}
-            image={c.image}
-            name={c.name}
-            birthday={c.birthday}
-            gender={c.gender}
-            job={c.job}
-            />
-          )
-        })
-      }
-    </div>
-  );
+class App extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />) })}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
 
-export default App;
-
+export default withStyles(styles)(App);
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,43 +1,27 @@
 // import 특정한 라이브러리를 불러올 때 사용
-
 // export 내보내기 (다른 파일에서 사용할 수 있도록)
 
 // jsx 문법에서 2개이상 사용시 반드시 div와 같은 태그들로 감싸줘야 함 (안하면 오류뜸)
 
 // map함수를 이용해서 반복문을 사용 가능 (map 사용 시 key값을 이용해 구문가능하게 만들어 줘야 함)
 
+// Material UI 사용해보기
+
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 class Customer extends React.Component { // 리엑트에 컴포넌트 형태로 작성이 된 클래스를 상속받아서 Customer 클래스 정의하기
     render() { // render()는 실제로 그려지는 내용 담는 곳, props를 이용해서 받은 값 이용하기 (this)
         return (
-            <div>
-                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
-                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
-            </div>
-        )
-    }
-}
-
-class CustomerProfile extends React.Component { // alt은 알려주는거
-    render() {
-        return (
-            <div>
-                <img src={this.props.image} alt="profile"/>
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
-
-class CustomerInfo extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile" /></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
         )
     }
 }
