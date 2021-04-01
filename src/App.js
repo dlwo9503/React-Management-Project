e2c89cb8e0adcb0d4fa760f3676dfda874602df5
Lee Jae-Seong <74041698+dlwo9503@users.noreply.github.com>
2021-04-02 01:16:34 +0900

    배열 및 map 사용

--- src/App.js
+++ src/App.js
@@ -2,21 +2,52 @@ import logo from './logo.svg';
 import './App.css';
 import Customer from './components/Customer'; // export 한 Customer 불러오기
 
-const customer = {
+const customers = [
+{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
   'name': '이재성',
   'birthday': '950328',
   'gender': '남자',
   'job': '개발자'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '김지영',
+  'birthday': '920825',
+  'gender': '여자',
+  'job': '공무원'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '이재영',
+  'birthday': '920722',
+  'gender': '남자',
+  'job': '자영업자'
 }
+]
 
 function App() {
   return (
-    <Customer // 컴포넌트 사용
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}
-    />
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+            />
+          )
+        })
+      }
+    </div>
   );
 }
 
