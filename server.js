41fe7c6703d63345541d96de333de334fedf658d
Lee Jae-Seong <74041698+dlwo9503@users.noreply.github.com>
2021-04-05 13:58:34 +0900

    Customer 테이블 생성 및 Node.js DB 연결

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs'); // 파일에 접근 가능하게하는 라이브러리
 const express = require('express'); // express 불러오기
 const bodyParser = require('body-Parser'); // body-Parser 불러오기
 const app = express(); // express 사용
@@ -6,34 +7,27 @@ const port = process.env.Port || 5000; // port 정의 - 5000
 app.use(bodyParser.json()); // json의 형태로 사용하겠다
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://placeimg.com/64/64/1',
-            'name': '이재성',
-            'birthday': '950328',
-            'gender': '남자',
-            'job': '개발자'
-          },
-          {
-            'id': 2,
-            'image': 'https://placeimg.com/64/64/2',
-            'name': '김지영',
-            'birthday': '920825',
-            'gender': '여자',
-            'job': '공무원'
-          },
-          {
-            'id': 3,
-            'image': 'https://placeimg.com/64/64/3',
-            'name': '이재영',
-            'birthday': '920722',
-            'gender': '남자',
-            'job': '자영업자'
-          }
-    ])
-})
+    connection.query(
+      "SELECT * FROM opentutorials.CUSTOMER",
+      (err, rows, fields) => {
+        res.send(rows);
+      }
+    );
+});
 
 // app.get('/api/hello', (req, res) => {
 //     res.send({message: 'Hello Express!'});
