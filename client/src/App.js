43ba8ebf725dcb162cd4e0e88747bc92d6102183
Lee Jae-Seong <74041698+dlwo9503@users.noreply.github.com>
2021-04-05 21:28:38 +0900

    App Bar, 한글 폰트 등 전체적으로 Material UI 적용

--- client/src/App.js
+++ client/src/App.js
@@ -17,17 +17,84 @@ import Paper from '@material-ui/core/Paper'; // 컴포넌트의 외부를 감싸
 import { Component } from 'react';
 import CirculerProgress from '@material-ui/core/CircularProgress';
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+
 const styles = theme => ({ // 머테리얼 유아이에다 css 같이 사용
   root: { // root class css
     width: '100%',
-    marginTop: theme.spacing.unit * 3,
-    overflowX: "auto" // x축으로 오버플로우가 발생할 수 있도록 처리하는거임
+    minWidth: 1080
   },
-  table: { // table class css
-    minWidth: 1080 // 테이블은 적어도 1080px만큼 자리를 차지하도록 함, 이보다 작다면 가로 스크롤바가 나타남
+  menu: {
+    marginTop: 15,
+    marginBottom: 15,
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  tableHead: {
+    fontSize: '1.0rem'
+  },
+  paper: {
+    marginLeft: 18,
+    marginRight: 18
   },
   progress: {
     margin: theme.spacing.unit * 2
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
   }
 })
 
@@ -58,59 +125,91 @@ class App extends Component {
       completed: 0
     });
     this.callApi()
-      .then(res => this.setState({customers: res})) // 받아온 데이터를 customers에 저장
+      .then(res => this.setState({ customers: res })) // 받아온 데이터를 customers에 저장
       .catch(err => console.log(err)); // 에러 처리
   }
 
   componentDidMount() {
     this.timer = setInterval(this.progress, 20); // 타이머를 이용해서 0.02초마다 progress함수가 수행될 수 있도록 처리
     this.callApi()
-      .then(res => this.setState({customers: res})) // 받아온 데이터를 customers에 저장
+      .then(res => this.setState({ customers: res })) // 받아온 데이터를 customers에 저장
       .catch(err => console.log(err)); // 에러 처리
   }
 
-  callApi = async() => { // 비동기적으로 어떠한 내용을 수행하기 위함
+  callApi = async () => { // 비동기적으로 어떠한 내용을 수행하기 위함
     const response = await fetch('/api/customers'); // 접속하고자 하는 api의 주소를 넣음
     const body = await response.json(); // 고객의 목록이 json 형태로 출력이 되는데 그것을 body라는 변수에 넣어줌
     return body; // body를 반환
   }
 
+  handleValueChange = (e) => {
+    let nextState = {};
+    nextState[e.target.name] = e.target.value;
+    this.setState(nextState);
+  }
+
   progress = () => {
-    const {completed} = this.state;
-    this.setState({completed: completed >= 100 ? 0 : completed + 1}); // completed가 100이 되는 순간 0으로 줄어들고 아닐시 +1
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1 }); // completed가 100이 되는 순간 0으로 줄어들고 아닐시 +1
   }
 
   render() {
     const { classes } = this.props;
+    const cellList = ["번호", "프로필 이미지", "이름", "생년월일", "성별", "직업", "설정"];
     return (
-      <div>
-        <Paper className={classes.root}>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="open drawer">
+              <MenuIcon />
+            </IconButton>
+            <Typography className={classes.title} variant="h6" noWrap>
+              고객 관리 시스템
+          </Typography>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="검색하기"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </div>
+          </Toolbar>
+        </AppBar>
+        <div className={classes.menu}>
+          <CustomerAdd stateRefresh={this.stateRefresh} />
+        </div>
+        <Paper className={classes.paper}>
           <Table className={classes.table}>
             <TableHead>
               <TableRow>
-                <TableCell>번호</TableCell>
-                <TableCell>이미지</TableCell>
-                <TableCell>이름</TableCell>
-                <TableCell>생년월일</TableCell>
-                <TableCell>성별</TableCell>
-                <TableCell>직업</TableCell>
-                <TableCell>설정</TableCell>
+                {cellList.map(c => {
+                  return <TableCell className={classes.tableHead}>{c}</TableCell>
+                })}
               </TableRow>
             </TableHead>
             <TableBody>
               {this.state.customers ? this.state.customers.map(c => { // customers에 데이터가 있다면? true : false
                 return (<Customer stateRefresh={this.stateRefresh} key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)
               }) :
-              <TableRow>
-                <TableCell colSpan="6" align="center">
-                  <CirculerProgress className={classes.progress} variant="indeterminate" value={this.state.completed} />
-                </TableCell>
-              </TableRow>
+                <TableRow>
+                  <TableCell colSpan="6" align="center">
+                    <CirculerProgress className={classes.progress} variant="indeterminate" value={this.state.completed} />
+                  </TableCell>
+                </TableRow>
               }
             </TableBody>
           </Table>
         </Paper>
-        <CustomerAdd stateRefresh={this.stateRefresh}/>
       </div>
     );
   }
